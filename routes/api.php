676aefc2dc33e6e28985cdc519ab8d9ae9e9930a
pedro0419 <pedro.estacio2@aluno.ce.gov.br>
2026-02-25 20:14:58 -0300

    feat:API-Login adicionada

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,19 @@
 
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts', [PostController::class, 'index']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
 
 ?>
