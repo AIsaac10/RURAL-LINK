7b5d3ca807de31d57d1f5759f81aa3500a72346f
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2026-02-24 23:48:37 -0300

    Feat: Corrects the PostController namespace in the routes file.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php 
 
-use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
 
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts', [PostController::class, 'index']);
+
 ?>
