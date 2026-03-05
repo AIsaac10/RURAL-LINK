d60c66f8bb99b12b57ae066febf53623a7625682
AIsaac10 <isaacmoreira12332@gmail.com>
2026-03-05 20:54:29 -0300

    Feat: Post model and migration

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,5 +15,6 @@ class Post extends Model
         'price',
         'location',
         'producer_type',
+        'user_id'
     ];
 }
---
+++ database/migrations/2026_03_05_203431_create_posts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+
+            // relação com usuário
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+
+            $table->string('title');
+            $table->text('description');
+
+            $table->decimal('price', 10, 2)->nullable();
+            $table->string('location')->nullable();
+
+            $table->enum('producer_type', [
+                'autonomous',
+                'organic',
+                'corporate',
+                'company'
+            ])->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
