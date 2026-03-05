fb878b51e20509a83c45f88daf38ebbb2a1c0284
AIsaac10 <isaacmoreira12332@gmail.com>
2026-03-05 20:55:20 -0300

    Feat: api routes

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,44 @@
-<?php 
+<?php
 
-use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PostController;
 
-Route::post('/posts', [PostController::class, 'store']);
-Route::get('/posts', [PostController::class, 'index']);
+/*
+|--------------------------------------------------------------------------
+| Rotas públicas (não precisam de login)
+|--------------------------------------------------------------------------
+*/
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-});
+Route::get('/posts', [PostController::class, 'index']); // listar posts
 
-?>
+
+/*
+|--------------------------------------------------------------------------
+| Rotas protegidas (precisam de token)
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    // usuário logado
+    Route::get('/user', function (Request $request) {
+        return response()->json($request->user());
+    });
+
+    // logout
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // criar post
+    Route::post('/posts', [PostController::class, 'store']);
+
+    // atualizar post
+    Route::put('/posts/{id}', [PostController::class, 'update']);
+
+    // deletar post
+    Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+});
